0ce45aaa46e05b8f53e1f28dcb277b85db2105fb
Philipp Klaus <philipp.klaus@gmail.com>
2014-11-12 16:15:12 +0100

    Adding event_id to the output DataFrame
    * This column states for each particle which event it belongs to.
    * If you don't want the column to be determined and included in
      your DataFrame, specify --no-event-id-column on the command line.

--- read_urqmd_pandas.py
+++ read_urqmd_pandas.py
@@ -19,9 +19,22 @@ class F14_Reader(object):
         event = None
         names = ['r0', 'rx', 'ry', 'rz', 'p0', 'px', 'py', 'pz', 'm', 'ityp', '2i3', 'chg', 'lcl#', 'ncl', 'or']
         df = pd.read_table(self.data_file, names=names, delim_whitespace=True)
+        if self.add_event_id_column:
+            #total_event_no = len(df[df.r0 == 'UQMD'])
+            df['event_id'] = 0
+            event_start = None
+            for idx in df[df.r0 == 'event#'].index:
+                if event_start == None:
+                    event_start = idx
+                    continue
+                df.loc[event_start:idx, 'event_id'] = df.loc[event_start, 'rx']
+                event_start = idx
+            df.loc[event_start:, 'event_id'] = df.loc[event_start, 'rx']
         df = df[df['or'].notnull()]
         df = df.convert_objects(convert_numeric=True)
         df.dropna(how='any', inplace=True)
+        if self.add_event_id_column:
+            df['event_id'] = df['event_id'].astype(np.int64)
         df['ityp'] = df['ityp'].astype(np.int64)
         df['2i3'] = df['2i3'].astype(np.int64)
         df['chg'] = df['chg'].astype(np.int64)
@@ -35,9 +48,10 @@ def main():
     parser = argparse.ArgumentParser(description='Read a config file.')
     parser.add_argument('urqmd_file', metavar='URQMD_FILE', type=argparse.FileType('r', encoding='ascii'), help="Must be of type .f14")
     parser.add_argument('--output-file', metavar='OUT_FILE', help='The .pkl file to store the information in')
+    parser.add_argument('--no-event-id-column', action='store_false', help='Include a column event_id in the pandas DataFrame.')
     args = parser.parse_args()
 
-    df = F14_Reader(args.urqmd_file).get_dataframe()
+    df = F14_Reader(args.urqmd_file, args.no_event_id_column).get_dataframe()
 
     if args.output_file:
         df.to_pickle(args.output_file)
